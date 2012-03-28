ca527f4e2b5143312b1733a9186529539b005929
philippkueng <git@philippkueng.ch>
2012-03-28 12:34:37 +0200

    fixed train line extraction script

--- scrapers/extract_train_lines.rb
+++ scrapers/extract_train_lines.rb
@@ -1,42 +1,49 @@
 require 'nokogiri'
 require 'json'
+require 'sequel'
+
+# DB = Sequel.sqlite # memory database
+DB = Sequel.connect('mysql://root:@127.0.0.1:3306/trainsharing')
+
+DB.create_table :lines do
+  primary_key :id
+  String :name
+  String :link
+end
+
+lines = DB[:lines] # Create a dataset
 
 files = Dir.new("data").entries
 
-result_hash = {}
+# result_hash = {}
 counter = 1
 
 for file in files
-	if /\.html/.match(file) and not /error/.match(file)
-		
-		puts "working on [#{counter}/#{files.length}]"
-		counter += 1
+  if /\.html/.match(file) and not /error/.match(file)
+    
+    puts "working on [#{counter}/#{files.length}]"
+    counter += 1
 
-		f = File.open("data/#{file}")
-		doc = Nokogiri::HTML(f)
-		f.close
+    f = File.open("data/#{file}")
+    doc = Nokogiri::HTML(f)
+    f.close
 
-		for tr in doc.search("table.hfs_stboard tr")
-			a_time = tr.search("td.journey a")
+    for tr in doc.search("table.hfs_stboard tr")
+      a_time = tr.search("td.journey a")
 
-			if a_time != nil and a_time.first != nil
-				span_time = a_time.search("span")
+      if a_time != nil and a_time.first != nil
+        span_time = a_time.search("span")
 
-				if span_time != nil and span_time.first != nil
-					result_hash[span_time.first.children[0]] = a_time.first.attributes['href'].value
-				end
-			end
-		end
-
-		File.open("json_data/#{counter}.json", "w") do |t|
-			t.write(result_hash.to_json)
-			result_hash = {}
-		end
-
-	end
+        if span_time != nil and span_time.first != nil and span_time.first.children[0] != nil
+          
+          # Check if line is already in DB and insert of not in there.
+          query = lines.filter(:name => span_time.first.children[0].to_s)
+          if query.count == 0
+            lines.insert(:name => span_time.first.children[0].to_s, :link => a_time.first.attributes['href'].value.to_s)
+          end
+          
+        end
+      end
+    end
+  end
 end
-
-# File.open("data/train_lines.json", "w") do |t|
-# 	t.write(result_hash.to_json)
-# end
-
