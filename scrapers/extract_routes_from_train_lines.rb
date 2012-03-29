0d2dd05cb961e5f2dc915f0e4c52437ab6173a13
philippkueng <git@philippkueng.ch>
2012-03-29 23:02:40 +0200

    added extract routes from train line dump script

--- scrapers/extract_routes_from_train_lines.rb
+++ scrapers/extract_routes_from_train_lines.rb
@@ -22,7 +22,7 @@ counter = 1
 for file in files
   if /\.html/.match(file) and not /error/.match(file)
     
-    puts "working on [#{counter}/#{files.length}]"
+    puts "working on [#{counter}/#{files.length}] - #{file}"
     counter += 1
     
     f = File.open("lines/#{file}")
@@ -33,76 +33,37 @@ for file in files
     
     # Get train line number.    
     for div in doc.search("div.hafas div.hac_greybox div b")
-      train_number = div.children.to_s
-      # puts "-> #{train_number}"
+      train_number = div.children.text
     end
     
     last_dep_station = nil
     last_dep_time = nil
     
+    # puts file
+    
     for tr in doc.search("table.hfs_traininfo tr")      
-      if tr.attributes["class"] != nil
+      if tr.attributes["class"] != nil and tr.search("td.location a")[0] != nil
         if last_dep_station != nil and last_dep_time != nil # has already a starting station
-
-          current_station = tr.search("td.location a")[0].children.to_s
-
-          # puts "-> inserting route from #{last_dep_station} to #{current_station}"
+          
+          current_station = tr.search("td.location a")[0].children.text
 
           routes.insert(
             :linename => train_number,
             :dep_station => last_dep_station,
             :dep_time => last_dep_time,
             :arr_station => current_station,
-            :arr_time => tr.search("td.arr.time")[0].children.to_s.gsub(/\n/, "")
+            :arr_time => tr.search("td.arr.time")[0].children.text.gsub(/\n/, "")
           )
 
           last_dep_station = current_station
-          last_dep_time = tr.search("td.dep.time")[0].children.to_s.gsub(/\n/, "")
+          last_dep_time = tr.search("td.dep.time")[0].children.text.gsub(/\n/, "")
 
         else
-          last_dep_station = tr.search("td.location a")[0].children.to_s
-          last_dep_time = tr.search("td.dep.time")[0].children.to_s.gsub(/\n/, "")
-          # puts last_dep_station
-          # puts last_dep_time
-
+          last_dep_station = tr.search("td.location a")[0].children.text
+          last_dep_time = tr.search("td.dep.time")[0].children.text.gsub(/\n/, "")
         end
       end
     end
     
   end
 end
-
-# files = Dir.new("data").entries
-# 
-# # result_hash = {}
-# counter = 1
-# 
-# for file in files
-#   if /\.html/.match(file) and not /error/.match(file)
-#     
-#     puts "working on [#{counter}/#{files.length}]"
-#     counter += 1
-# 
-#     f = File.open("data/#{file}")
-#     doc = Nokogiri::HTML(f)
-#     f.close
-# 
-#     for tr in doc.search("table.hfs_stboard tr")
-#       a_time = tr.search("td.journey a")
-# 
-#       if a_time != nil and a_time.first != nil
-#         span_time = a_time.search("span")
-# 
-#         if span_time != nil and span_time.first != nil and span_time.first.children[0] != nil
-#           
-#           # Check if line is already in DB and insert of not in there.
-#           query = lines.filter(:name => span_time.first.children[0].to_s)
-#           if query.count == 0
-#             lines.insert(:name => span_time.first.children[0].to_s, :link => a_time.first.attributes['href'].value.to_s)
-#           end
-#           
-#         end
-#       end
-#     end
-#   end
-# end
