3b5c4633ad69ee34471f589c5dccd994f934ddd8
Philipp Küng <i@philippkueng.ch>
2012-03-30 20:21:40 +0200

    cleaned up, removed unused config_sample.js and moved stations.json to the scrapers folder

--- config_sample.js
+++
@@ -1,7 +0,0 @@
-exports.mysql = {
-    database: 'trainsharing',
-    user: 'your username',
-    password: 'your password',
-    host: 'database machine ip',
-    port: 3306
-};
--- sample_test.js
+++
@@ -1,7 +0,0 @@
-var request = require('request');
-
-for(var i = 0; i < 10000; i++){
-  request('http://localhost:3000', function(error, response, body){
-    console.log(body);
-  });
-}
