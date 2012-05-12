a3160199eacdaf97577a816f78d770a7ac6ad6c5
Gregor Wegberg <gregor@wegberg.ch>
2012-05-12 18:37:42 +0200

    moved Neo4J URL setting into config.js

--- config.js
+++ config.js
@@ -11,5 +11,8 @@ module.exports = {
         database: process.env.MYSQL_DATABASE ? process.env.MYSQL_DATABASE : 'trainsharing',
         port: process.env.MYSQL_PORT ? process.env.MYSQL_PORT : 3306,
         host: process.env.MYSQL_HOST ? process.env.MYSQL_HOST : 'localhost'
+    },
+    neo4j: {
+        url: process.env.NEO4J_URL || "http://localhost:7474"
     }
 }
