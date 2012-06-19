7c3d8681631d4bbaf35e3cf2fc1e1fca77fb2881
philippkueng <git@philippkueng.ch>
2012-06-19 23:49:05 +0200

    moved the configuration checking to the config.js file altogether

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     twitter: {
-        consumer_key: "",
-        consumer_secret: "",
+        consumer_key: process.env.TWITTER_CONSUMER_KEY || "",
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET || "",
         access_token: "",
         acces_token_secret: ""
     },
